e7111e0879c2705833e731de12ab14113f16d8df
johyeon yoon <yds03080@naver.com>
2021-07-28 21:01:25 +0900

    Initial Setting for project

---
+++ Landmerks/Landmerks/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Landmerks
+//
+//  Created by Johyeon Yoon on 2021/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+/*
+ By default, SwiftUI view files declare two structures.
+ The first structure conforms to the View protocol and describes the view’s content and layout.
+ The second structure declares a preview for that view.
+ */
---
+++ Landmerks/Landmerks/LandmerksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmerksApp.swift
+//  Landmerks
+//
+//  Created by Johyeon Yoon on 2021/07/28.
+//
+
+import SwiftUI
+
+@main  // 앱의 entry point를 나타냄 
+struct LandmerksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
