669767669fb7b1a10aa71c1e1afdf6f2d65d6bc3
johyeon yoon <yds03080@naver.com>
2021-07-28 21:21:39 +0900

    Customize the TextView

--- Landmerks/Landmerks/ContentView.swift
+++ Landmerks/Landmerks/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.black)
+        
     }
 }
 
@@ -20,9 +22,21 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+// SwiftUI에서 inspector를 여는 것은 cmd + click
 
 /*
  By default, SwiftUI view files declare two structures.
  The first structure conforms to the View protocol and describes the view’s content and layout.
  The second structure declares a preview for that view.
+ */
+
+
+/*
+ To customize a SwiftUI view, you call methods called modifiers.
+ 
+ Modifiers wrap a view to change its display or other properties.
+ 
+ Each modifier returns a new view, so it’s common to chain multiple modifiers, stacked vertically.
+
+
  */
